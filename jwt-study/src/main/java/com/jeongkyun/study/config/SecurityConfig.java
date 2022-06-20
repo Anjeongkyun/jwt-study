84fe6d30d23f16dc4b70e8b4e33b8129af736aba
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-20 23:59:39 +0900

    Security Config 수정 및 H2 키워드 에러 수정을 위해 User -> Users 수정, defer-datasource-initialization: true 추가

--- jwt-study/src/main/java/com/jeongkyun/study/config/SecurityConfig.java
+++ jwt-study/src/main/java/com/jeongkyun/study/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.jeongkyun.study.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //web 보안 활성화
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //h2-console 하위 모든 요청과 파비콘은 모두 무시하도록 설정
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+             web.ignoring()
+                .antMatchers("/h2-console/**"
+                ,"/favicon.ico");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
